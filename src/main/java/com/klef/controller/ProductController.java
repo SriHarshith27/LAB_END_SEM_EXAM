b95a76e61d0e778208c7a6f9822acf33ffd831e7
Harshith <147683599+SriHarshith27@users.noreply.github.com>
2024-12-07 11:54:51 +0530

    commit

---
+++ src/main/java/com/klef/controller/ProductController.java
@@ -0,0 +1,26 @@
+package com.klef.controller;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.klef.model.Product;
+import com.klef.service.ProductService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+}
---
+++ src/main/java/com/klef/service/ProductService.java
@@ -0,0 +1,33 @@
+package com.klef.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.klef.model.Product;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private final String API_URL = "https://fakestoreapi.com/products"; // Fake Store API URL
+    
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public List<Product> getAllProducts() {
+        // Updated code to avoid deprecated method
+        UriComponentsBuilder builder = UriComponentsBuilder.newInstance()
+                .scheme("https")
+                .host("fakestoreapi.com")
+                .path("/products");
+        
+        Product[] products = restTemplate.getForObject(builder.toUriString(), Product[].class);
+        return Arrays.asList(products);
+    }
+}
+   
